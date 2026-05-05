03528fc2507330c9eaed03fbe96fa741997953bb
Carolina Melo <caroolpmelo@gmail.com>
2026-05-05 19:47:11 -0300

    Project start files

---
+++ CondingWiki_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CondingWiki_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
